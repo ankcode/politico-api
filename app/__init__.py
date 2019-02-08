c690a7bda44f65949625836f256a257bb902d11f
ankdev <mochogefrank01@gmail.com>
2019-02-08 15:39:16 +0300

    [Update #163735201] Update app module to allow heroku setup.

--- app/__init__.py
+++ app/__init__.py
@@ -6,7 +6,7 @@ from app.api.v1 import views
 
 def create_app(config_name):
     app = Flask(__name__, instance_relative_config=True)
-    app.config.from_object(app_config[config_name])
+    app.config.from_object(app_config['development'])
     app.config.from_pyfile('config.py')
     app.register_blueprint(views.BASE_URL_BP)
     
