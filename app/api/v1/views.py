ab612c826d3b2dfaa8f55afdc79b878bde58bd62
ankdev <mochogefrank01@gmail.com>
2019-02-08 11:28:25 +0300

    [Finishes #163735193] This endpoint fetches all the registered political offices.

--- app/api/v1/views.py
+++ app/api/v1/views.py
@@ -1,16 +1,19 @@
 from flask import Blueprint, jsonify, request, Response
-from app.api.v1.models import CreateParty, GetAllParties, GetSpecificParty, EditPartyName, DeleteParty, CreateOffice
+from app.api.v1.models import CreateParty, GetAllParties, GetSpecificParty, EditPartyName, DeleteParty, CreateOffice, GetAllOffices
 
 BASE_URL_BP = Blueprint("V1", __name__, url_prefix='/api/v1')
+
 
 def login(username, password):
     """ Checks for password and username  """
     return username == 'admin' and password == 'admin'
 
+
 def check_login():
     return Response(
-    'You must be logged in as an admin to create a party.', 401,
-    {'WWW-Authenticate': 'Basic realm="Login Required"'})
+        'You must be logged in as an admin to create a party.', 401,
+        {'WWW-Authenticate': 'Basic realm="Login Required"'})
+
 
 def requires_login(creds):
 
@@ -27,69 +30,81 @@ def requires_login(creds):
     return getCredentials
 
 
-@BASE_URL_BP.route('/parties', methods = ['POST'])
+@BASE_URL_BP.route('/parties', methods=['POST'])
 @requires_login
 def partiesCreate():
     """ Creates a new party if doesnot exist """
 
     if request.method == 'POST':
 
-        party_val =  request.get_json(force=True)
+        party_val = request.get_json(force=True)
 
         msg = CreateParty(party_val)
         return msg
 
-@BASE_URL_BP.route('/parties', methods = ['GET'])
+
+@BASE_URL_BP.route('/parties', methods=['GET'])
 def partiesGetAll():
     """ Fetches all parties """
 
     if request.method == 'GET':
         msg = GetAllParties()
-        
+
         return msg
 
-@BASE_URL_BP.route('/parties/<party_id>', methods = ['GET'])
+
+@BASE_URL_BP.route('/parties/<party_id>', methods=['GET'])
 def partiesGetSpecific(party_id):
     """ Fetches all parties """
 
     if request.method == 'GET':
 
         msg = GetSpecificParty(party_id)
-        
+
         return msg
 
-@BASE_URL_BP.route('/parties/<party_id>/name', methods = ['PATCH'])
+
+@BASE_URL_BP.route('/parties/<party_id>/name', methods=['PATCH'])
 def EditParty(party_id):
     """ Fetches all parties """
 
     if request.method == 'PATCH':
 
-        party_name =  request.get_json(force=True)
+        party_name = request.get_json(force=True)
 
         msg = EditPartyName(party_id, party_name)
-        
+
         return msg
 
-@BASE_URL_BP.route('/parties/<party_id>', methods = ['DELETE'])
+
+@BASE_URL_BP.route('/parties/<party_id>', methods=['DELETE'])
 def delParty(party_id):
     """ Delete a specific party """
 
     if request.method == 'DELETE':
 
         msg = DeleteParty(party_id)
-        
+
         return msg
 
-@BASE_URL_BP.route('/offices', methods = ['POST'])
+
+@BASE_URL_BP.route('/offices', methods=['POST'])
 def officeCreate():
     """ Creates a new office if doesnot exist """
 
     if request.method == 'POST':
 
-        office_val =  request.get_json(force=True)
+        office_val = request.get_json(force=True)
 
         msg = CreateOffice(office_val)
         return msg
 
 
+@BASE_URL_BP.route('/offices', methods=['GET'])
+def officesGetAll():
+    """ Fetches all offices """
 
+    if request.method == 'GET':
+        msg = GetAllOffices()
+
+        return msg
